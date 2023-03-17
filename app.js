7923b48d1653f13fe054ee4b411a8460af294b5e
Brayan Antonio <112229331+brayanalmengor04@users.noreply.github.com>
2023-03-17 04:17:28 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+function mostrarMenu(){    
+    document.getElementById('menumovil').style.transition ='all 1s';
+    document.getElementById('menumovil').style.display ='block';
+    document.getElementById('menumovil').style.left='0%';
+}
+
+function salirMenu (){
+    document.getElementById('menumovil').style.transition ='all 1s'; 
+    document.getElementById('menumovil').style.left='100%'; 
+}
+
+const fondo = document.querySelector('.container__sobremi-porfile')
+fondo.addEventListener('click', () => {
+  fondo.classList.remove('animate')
+    setTimeout(()=>{
+        fondo.classList.add('animate')
+    },100)
+});  
+const proyectos = document.querySelector('.container-proyectos')
+proyectos.addEventListener('click', () => {
+  proyectos.classList.remove('animate')
+    setTimeout(()=>{
+        proyectos.classList.add('animate')
+    },100)
+}); 
+ 
+function mostrar (){
+    document.querySelector('.proyectos p').style.visibility='visible';
+} 
+
+function noMostrar(){
+    document.querySelector('.proyectos p').style.visibility='hidden';
+}
+
